cb4a6ea5d8cbaa2eafe8831181068b417cce0572
soumyasahoosilu <sahoosoumyasilu2001@gmail.com>
2024-08-31 22:50:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [advice, setAdvice] = useState("");
+
+  // Function to fetch advice from the API
+  const fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        setAdvice(response.data.slip.advice); // Update the state with the fetched advice
+      })
+      .catch((error) => {
+        console.error("Error fetching advice:", error);
+      });
+  };
+
+  useEffect(() => {
+    console.log("Component Did Mount");
+    fetchAdvice(); // Fetch advice once on mount
+  }, []); // Empty dependency array means this effect runs once, similar to componentDidMount
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{advice}</h1>
+          <button className="button" onClick={fetchAdvice}>
+            <span>Give Me Advice!</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+
+
